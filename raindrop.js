ee9a1ebe835b17a64f59a08dc5393e04055e8ff1
sristi3456 <73381822+sristi3456@users.noreply.github.com>
2021-05-26 12:38:48 +0530

    Add files via upload

---
+++ raindrop.js
@@ -0,0 +1,27 @@
+class DROP {
+    constructor(x, y) {
+        var options = {
+            restitution: 1,
+            friction: 0,
+            isStatic:true
+        }
+        this.r = 10;
+        this.body = Bodies.circle(x, y, 5, options);
+        
+        World.add(world, this.body);
+    }
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x, pos.y);
+        rotate(angle);
+        noStroke();
+        fill("blue");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+};
